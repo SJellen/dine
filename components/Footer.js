608db9a9fd528f6f8c74d46565ba73519221e73d
SJellen <sjellen@me.com>
2022-01-02 12:02:32 -0500

    layout of footer containers

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,28 @@
 import styles from '../styles/Footer.module.scss'
+import Image from 'next/image'
 
 export default function Footer() {
     return (
         <footer className={styles.container}>
-            footer
+            <div className={styles.leftContainer}>
+                <Image src="/assets/logo.svg" alt="icon" width="103" height="40" />
+            </div>
+            <div className={styles.rightContainer}>
+                <div className={styles.address}>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div className={styles.hours}>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
         </footer>
     )
 }
