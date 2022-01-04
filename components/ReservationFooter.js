5288769242b60d1e2aeb0296dfdcd92c7ac70eef
SJellen <sjellen@me.com>
2022-01-04 13:54:54 -0500

    reservation footer set down to mobile

--- components/ReservationFooter.js
+++ components/ReservationFooter.js
@@ -1,10 +1,15 @@
 import styles from '../styles/ReservationFooter.module.scss'
+import Link from 'next/link'
 
 export default function ReservationFooter() {
 
     return (
         <div className={styles.container}>
             <div className={styles.flexContainer}>
+                <h2>Ready to make a reservation?</h2>
+                <Link href="/" passHref >
+                    <a>BOOK A TABLE</a>
+                </Link>
                 
             </div>
         </div>
