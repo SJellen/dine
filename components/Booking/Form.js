d7d99325a27427623391a998b7713d146c82579b
SJellen <sjellen@me.com>
2022-01-05 14:44:43 -0500

    positioning on header and booking/hero section

--- components/Booking/Form.js
+++ components/Booking/Form.js
@@ -4,7 +4,6 @@ export default function Form() {
     
     return (
         <div className={styles.container}>
-            Form
         </div>
     )
 }
--- components/Booking/Hero.js
+++ components/Booking/Hero.js
@@ -4,7 +4,10 @@ export default function Hero() {
     
     return (
         <div className={styles.container}>
-            Hero
+            <div className={styles.textContainer}>
+                <h2>Reservations</h2>
+                <p>We can’t wait to host you. If you have any special requirements please feel free to call on the phone number below. We’ll be happy to accommodate you.</p>
+            </div>
         </div>
     )
 }
