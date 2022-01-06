44de166403c8e29d3cf864b80d17330b2bfc3a7a
SJellen <sjellen@me.com>
2022-01-06 13:42:20 -0500

    start of work on Home/Hero section

---
+++ components/Home/Hero.js
@@ -0,0 +1,11 @@
+
+import styles from '../../styles/Home/Hero.module.scss'
+
+export default function Hero() {
+
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import Hero from '../components/Home/Hero'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Hero />
     </div>
   )
 }
