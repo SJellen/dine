99c875012973c0a866c6011ef91d54f49c2b1241
SJellen <sjellen@me.com>
2022-01-06 11:12:39 -0500

    work on spacing the imput section

--- components/Booking/Form.js
+++ components/Booking/Form.js
@@ -4,6 +4,18 @@ export default function Form() {
     
     return (
         <div className={styles.container}>
+            <div></div>
+            <div>
+                <form>
+                    <input />
+                    <input />
+                    <input />
+                    <input />
+                    <input />
+                    <input />
+                    <input />
+                </form>
+            </div>
         </div>
     )
 }
