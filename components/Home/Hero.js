11451fa47edc36d190b21a93df2bbbbff9658e84
SJellen <sjellen@me.com>
2022-01-06 14:51:12 -0500

    desktop on Home/Hero layout

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -1,11 +1,19 @@
 
 import styles from '../../styles/Home/Hero.module.scss'
+import Link from 'next/link'
 
 export default function Hero() {
 
     return (
         <div className={styles.container}>
-
+            <div className={styles.backColor}></div>
+            <div className={styles.textContainer}>
+                <h1>Exquisite dining since 1989</h1>
+                <p>Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.</p>
+                <Link href="/book" passHref >
+                    <a>BOOK A TABLE</a>
+                </Link>
+            </div>
         </div>
     )
 }
