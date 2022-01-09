b84fa522f83879170c23140feb15c5de4ed9a8df
SJellen <sjellen@me.com>
2022-01-09 10:42:44 -0500

    text layout for Home/ mid sections

--- components/Home/Enjoyable.js
+++ components/Home/Enjoyable.js
@@ -5,7 +5,11 @@ export default function Enjoyable() {
 
     return (
         <div className={styles.container}>
-            enjoable
+            <div className={styles.imageContainer} ></div>
+            <div className={styles.textContainer} >
+                <h2>Enjoyable place for all the family</h2>
+                <p>Our relaxed surroundings make dining with us a great experience for everyone. We can even arrange a tour of the farm before your meal.</p>
+            </div>
         </div>
     )
 }
--- components/Home/Locally.js
+++ components/Home/Locally.js
@@ -5,7 +5,11 @@ export default function Locally() {
 
     return (
         <div className={styles.container}>
-            locally
+            <div className={styles.textContainer} >
+                <h2>The most locally sourced food</h2>
+                <p>All our ingredients come directly from our farm or local fishery. So you can be sure that you’re eating the freshest, most sustainable food.</p>
+            </div>
+            <div className={styles.imageContainer} ></div>
         </div>
     )
 }
