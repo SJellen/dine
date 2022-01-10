3ff8744c62adef674ce6f558c40e3f6422e5d265
SJellen <sjellen@me.com>
2022-01-10 13:04:51 -0500

    container layout

--- components/Home/Switch.js
+++ components/Home/Switch.js
@@ -5,9 +5,21 @@ export default function Switcher() {
 
     return (
         <div className={styles.container}>
-            <div className={styles.imgContainer}></div>
-            <div className={styles.textContainer}></div>
-            <div className={styles.selectionContainer}></div>
+            <div className={styles.imgContainer}>
+                <div className={styles.familyImage}></div>
+                <div className={styles.specialImage}></div>
+                <div className={styles.socialImage}></div>
+            </div>
+            <div className={styles.textContainer}>
+                <div className={styles.familyText}></div>
+                <div className={styles.specialText}></div>
+                <div className={styles.socialText}></div>
+            </div>
+            <div className={styles.selectionContainer}>
+                <div className={styles.familySelection}></div>
+                <div className={styles.specialSelection}></div>
+                <div className={styles.socialSelection}></div>
+            </div>
         </div>
     )
 }
