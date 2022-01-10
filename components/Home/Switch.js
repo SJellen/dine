36e63905405a43f8419a958d8d75c68e3e4c2dc2
SJellen <sjellen@me.com>
2022-01-10 13:14:14 -0500

    text on selection container

--- components/Home/Switch.js
+++ components/Home/Switch.js
@@ -1,5 +1,6 @@
-
 import styles from '../../styles/Home/Switcher.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Switcher() {
 
@@ -14,23 +15,35 @@ export default function Switcher() {
                 <div className={styles.familyText}>
                     <h2>Family Gathering</h2>
                     <p>We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.</p>
-                    <a>BOOK A TABLE</a>
+                    <Link href="/book" passHref >
+                        <a>BOOK A TABLE</a>
+                    </Link>
                 </div>
                 <div className={styles.specialText}>
                     <h2>Special Events</h2>
                     <p>Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.</p>
-                    <a>BOOK A TABLE</a>
+                    <Link href="/book" passHref >
+                        <a>BOOK A TABLE</a>
+                    </Link>
                 </div>
                 <div className={styles.socialText}>
                     <h2>Social Events</h2>
                     <p>Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.</p>
-                    <a>BOOK A TABLE</a>
+                    <Link href="/book" passHref >
+                        <a>BOOK A TABLE</a>
+                    </Link>
                 </div>
             </div>
             <div className={styles.selectionContainer}>
-                <div className={styles.familySelection}></div>
-                <div className={styles.specialSelection}></div>
-                <div className={styles.socialSelection}></div>
+                <div className={styles.familySelection}>
+                    <h4>FAMILY GATHERING</h4>
+                </div>
+                <div className={styles.specialSelection}>
+                    <h4>SPECIAL EVENTS</h4>
+                </div>
+                <div className={styles.socialSelection}>
+                    <h4>SOCIAL EVENTS</h4>
+                </div>
             </div>
         </div>
     )
